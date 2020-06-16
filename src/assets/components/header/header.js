ed17fb4eac132c8633baa5ae1e21cbe2f5a1f313
BlazarQSO <BlazarQSO@gmail.com>
2020-06-16 16:33:13 +0300

    feat: add the initial styles

--- src/assets/components/header/header.js
+++ src/assets/components/header/header.js
@@ -1,5 +1,17 @@
 import Games from '../games/games';
 
-export default function header() {
+function eventNav() {
   new Games().create('savannah');
+}
+
+export default function header() {
+  const btn = document.getElementById('headerBtn');
+  btn.addEventListener('click', (e) => {
+    btn.classList.toggle('header__click');
+    document.getElementById('nav').classList.toggle('nav__show');
+    e.stopPropagation();
+  });
+
+  const nav = document.getElementById('nav');
+  nav.addEventListener('click', eventNav);
 }
