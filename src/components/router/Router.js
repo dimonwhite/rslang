bcca1458c3a30f2f628d9bac74f86aa6749f5f6b
dimonwhite <gen-tver@yandex.ru>
2020-06-22 00:03:36 +0300

    feat: add change active link

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -13,6 +13,7 @@ export default class Router {
       dictionary: DictionaryController,
       statistics: StatisticsController,
     };
+    this.nav = document.getElementById('nav');
   }
 
   init() {
@@ -26,17 +27,29 @@ export default class Router {
   }
 
   changePage() {
-    this.mainSection.innerHTML = '';
-    this.mainSection.className = 'main';
-    document.body.className = 'body';
-    document.body.removeAttribute('style');
     this.parseHash();
-    this.currentPage = new this.pages[this.urlArray.shift()]();
-    this.currentPage.create(...this.urlArray);
+    const PageClass = this.pages[this.urlArray.shift()];
+    if (PageClass) {
+      this.mainSection.innerHTML = '';
+      this.mainSection.className = 'main';
+      document.body.className = 'body';
+      document.body.removeAttribute('style');
+      this.currentPage = new PageClass();
+      this.currentPage.create(...this.urlArray);
+      this.changeActiveLink();
+    }
   }
 
   parseHash() {
     const url = this.hash.replace('#/', '');
     this.urlArray = url ? url.split('/') : ['main'];
+  }
+
+  changeActiveLink() {
+    this.nav.querySelectorAll('.nav__list-item.active').forEach((listItem) => {
+      listItem.classList.remove('active');
+    });
+    const activeLink = this.nav.querySelector(`[href="/${this.hash}"]`);
+    activeLink.parentNode.classList.add('active');
   }
 }
