ef2f88828c7866a043c157df6ef82638bc009715
dimonwhite <gen-tver@yandex.ru>
2020-07-01 02:53:04 +0300

    feat: add styles and responsive

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './scss/main.scss';
 import Router from '@/components/router/Router';
+import { importAll } from '@/utils';
 import Header from './components/header/header';
 // eslint-disable-next-line no-unused-vars
 import HttpClient from './components/httpclient/HttpClient';
 
 require('./prototype.settings');
-
-function importAll(r) {
-  return r.keys().map(r);
-}
 
 importAll(require.context('./assets/img/', false, /\.svg$/));
 
