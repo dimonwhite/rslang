17332ad8dec90dd1b5493f7682ce484ff8d66a3d
dimonwhite <gen-tver@yandex.ru>
2020-07-12 20:51:53 +0300

    fix: remove error

--- src/data/personal.js
+++ src/data/personal.js
@@ -2,13 +2,15 @@ import dmitriyb from '../assets/img/aboutTeam/dmitriyb.png';
 import mihailc from '../assets/img/aboutTeam/mihailc.png';
 import timurk from '../assets/img/aboutTeam/timurk.jpg';
 import sergeyi from '../assets/img/aboutTeam/sergeyi.jpg';
+import wayjewish from '../assets/img/aboutTeam/wayjewish.jpg';
+import daricat from '../assets/img/aboutTeam/daricat.jpg';
 
 const personal = [
   {
     name: 'Дмитрий Белов',
     position: 'Капитан корабля',
     cv: 'Простой парень, которого в детстве с Земли похитили опустошители',
-    fund: 'Router, миниигра "SpeakIt"',
+    fund: 'Router, миниигра "SpeakIt", миниигра "Fillwords", промо страница',
     photo: dmitriyb,
   },
   {
@@ -29,8 +31,22 @@ const personal = [
     name: 'Ивков Сергей',
     position: 'Пилот',
     cv: 'Держу курс до Альфа Центавра',
-    fund: 'Мини-игра "Savannah", структура приложения, "Карточка" (гланвная страница)',
+    fund: 'Мини-игра "Savannah", структура приложения, "Карточка" (главная страница)',
     photo: sergeyi,
+  },
+  {
+    name: 'Дмитрий Уваров',
+    position: 'Бортинженер',
+    cv: 'Обычный биомеханический андроид с разбитым киберсердечком',
+    fund: 'Авторизация, Словарь',
+    photo: wayjewish,
+  },
+  {
+    name: 'Дарья Акалович',
+    position: 'Собираю инопланетные грибы',
+    cv: 'Готовлюсь к великому путешествию',
+    fund: 'Дизайн, Статистика',
+    photo: daricat,
   },
 ];
 
